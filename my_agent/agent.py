0202b9d0e2a6bd5cb3a1787a7fa83906d42460fb
Mahdi Mohsseni <109732366+MohsseniMahdi@users.noreply.github.com>
2026-04-18 22:39:23 +0200

    Update agent.py

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -25,17 +25,7 @@ gereedschap=[get_current_time],
 
 
 
- -verzoek op Soft te controleren
-#### om het pull-verzoek op Soft te controleren
-#### 
-#### om het pull-verzoek op Soft te controleren
-#### om het pull-verzoek op Soft te controleren
-
-
-
-
- op Soft te controleren
-
+ 
 
 
  
